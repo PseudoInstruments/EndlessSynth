b07e3dc4a5a8461c9e358f9c2171874044d20777
perevalovds <perevalovds@gmail.com>
2021-02-02 15:45:56 +0500

    generator of square wave using timer interrupt

--- 07_SquareWaveInterruptMega/07_SquareWaveInterruptMega.ino
+++ 07_SquareWaveInterruptMega/07_SquareWaveInterruptMega.ino
@@ -1,15 +1,14 @@
-//EndlessSynth, test of using Arduino Mega timer interrupts
+//EndlessSynth, generating square wave using timer interrupts.
 
-//This program runs TIMER2 at 44100 Hz, increase counter at each interrupt,
-//and prints real rate between calls and time.
-//Result: 
-//compare_match = ...-1; ~44400 Hz.
-//compare_match = ...; ~43480 Hz.
-
-
-//Based on https://www.instructables.com/Arduino-Timer-Interrupts/
+//Ports in Arduino https://www.arduino.cc/en/Reference/PortManipulation
+//Pins in Arduino Mega https://www.arduino.cc/en/Hacking/PinMapping2560
 
 //Device: Arduino Mega (Note: Not Uno!)
+//I use style of writing by using very basic Arduino functions,
+//in order code will be more readable by beginners.
+
+//Commutation:
+const byte pin_buz = 2;
 
 //Note: Monitor port baud must be set to 500000!
 
@@ -18,15 +17,23 @@ const long int audio_sample_rate = 44100;
 //---------------------------------------------------------------
 void setup() {
   Serial.begin(500000);
-  Serial.println("EndlessSynth Interrupt Test 1.1");
+  Serial.println("EndlessSynth Square Wave Interrupt Test 1.1 for Arduino Mega");
 
+  Serial.print("Buzzer pin: "); Serial.println(pin_buz);
+
+  //Set port/pin mode for Arduino Mega for buzzer
+  //https://www.arduino.cc/en/Hacking/PinMapping2560
+  //pin 2 is 4 bit of Port E (in scheme PE2)
+  DDRE = B00010000;    //pinMode(2,OUTPUT);
+
+  
+
+  //Set timer2 interrupt at "audio_sample_rate"
   int compare_match = 16000000 / (8*audio_sample_rate); //for timers0 and 2 must be 1..255, and for timer1 1..65535
   //compare_match = ...-1; ~44400 Hz.
   //compare_match = ...; ~43480 Hz.
   Serial.print("Interrupt compare match: "); Serial.println(compare_match);
 
-
-  //set timer2 interrupt at "audio_sample_rate"
   cli();    //stop interrupts
   TCCR2A = 0;   //set entire TCCR2A register to 0
   TCCR2B = 0;   //same for TCCR2B
@@ -38,12 +45,32 @@ void setup() {
   sei();    //start interrupts
 }
 
+
+//----------------------------------------------------------
+int freq = 440; //frequency of sound
+
+long int period_samples = audio_sample_rate / freq;
+long int period_samples2 = period_samples/2;
 //----------------------------------------------------------
 //process timer interrupt event
 long int t = 0;
 
+long int phase = 0;
+
 ISR(TIMER2_COMPA_vect) {
   t++;
+
+  //I noted that here should be no "/" or "%" operations,
+  //because timer is halted. As a workaround - use lower frame rate
+
+  phase++;
+  if (phase<period_samples2) {
+    PORTE=B00010000; //buzzer ON
+  }
+  else {    
+    PORTE=B00000000;  //buzzer OFF;
+  }
+  if (phase>=period_samples) phase = 0;
 }
 
 //----------------------------------------------------------
