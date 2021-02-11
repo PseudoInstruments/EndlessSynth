c705457d0a50565589303f869d3b73e52c1773a6
perevalovds <perevalovds@gmail.com>
2021-02-11 17:26:43 +0500

    sound input

---
+++ sound_input_tests/02_sound_passthrough_diffusion/02_sound_passthrough_diffusion.ino
@@ -0,0 +1,102 @@
+/* Program gets sound from microphone (A0)
+  and immediately outputs it to buzzer (pin 2) using diffusion thresholding algorithm,
+  see "sound_tests/12_PolyphonyMega_diffusion_thresholding" for the details on this
+  Can be used for electo-guitar connection too.
+
+  Controller: Arduino Uno
+
+  ----------------------------------------
+  Connection
+  ----------------------------------------
+  1) Microphone:
+  Take microphone unit for Arduino:
+  connect it to Gnd, 5V, and output signal connect to A0.
+
+  2) Potentiometer for pulling-up microphone:
+  Note, that microphone unit gives -2.5V...2.5V output,
+  so to digitize signal carefully we need to pull up it to 0..5V range for A0.
+  As a solution, I use compact 10KOm potentiometer,
+  connect left and right pins to Gnd, 5V, and output to A0 too.
+  Next, I adjust potentiometer to obtain 2.5V when Mic is in a silence.
+  Now microphone outputs 0..5V to A0!
+
+  3) Buzzer (or audio output) to pin 2.
+
+  ----------------------------------------
+  Programming details
+  ----------------------------------------
+  By default Arduino ADC works on 8Khz - ok for us.
+  But can speedup:
+  https://www.instructables.com/Make-Your-Own-Spy-Bug-Arduino-Voice-Recorder/
+  Recorder:
+  https://github.com/TMRh20/TMRpcm
+  http://microsin.net/programming/avr/real-time-digital-audio-processing-using-arduino.html
+
+*/
+
+const byte pin_buz = 2; //Buzzer pin
+
+//--------------------------------------------------------------
+void setup() {
+  Serial.begin(500000);
+  Serial.println("EndlessSynth Sound passtrough test (diffusion with thresholding), v. 1.1 for Arduino Uno or Mega");
+  Serial.println("Program gets sound from microphone (A0) and immediately outputs");
+  Serial.println("it to buzzer (pin 2) using diffusion algorithm.");
+  Serial.println("Note: please use potentiometer 10KOhm to move silence mic level to 2.5V");
+
+  //will be computed
+  //Serial.print("Audio sample rate: "); Serial.println(audio_sample_rate);
+
+  pinMode(pin_buz, OUTPUT); //activate buzzer
+
+}
+
+//--------------------------------------------------------------
+long int sound_value = 0;
+const int diff_step = //127 * POLYPHONY; //step of diffusion subtraction - 1..127, kind of threshold for sound
+  70; //127;    //just 127 - to make polyphony sounding more "phatty"
+
+const int thresh_sound = 10; //;  must be diff_step / 2, but for diff_keep <= 50 can be lower
+
+const int diff_keep = 30; //50;  //decaying diffusion in percents 0..100, 0 - no diffusion, 1 - keep all diffusion
+
+//--------------------------------------------------------------
+void loop() {
+
+  //run loop to measure time and deduce sample rate
+  int n = 1000;
+  int v;
+  unsigned long time0 = micros();
+  for (int i = 0; i < n; i++) {
+    //get sample
+    sound_value = sound_value * diff_keep / 100;  //decaying diffusion
+    sound_value += (analogRead(A0) >> 2) - 128; //0..1023 -> 0..255 -> -128..127
+    //output
+    if (sound_value >= thresh_sound) {
+      digitalWrite(pin_buz, HIGH); //buzzer ON
+      sound_value -= diff_step;       //diffusion propagation
+    }
+    else {
+      if (sound_value <= -thresh_sound) {
+        digitalWrite(pin_buz, LOW); //buzzer OFF;
+        sound_value += diff_step;       //diffusion propagation
+      }
+    }
+
+    //delay
+    delayMicroseconds(50); //10);  //need to adjust delay to achieve 8000Hz rate
+  }
+
+  //estimate sample rate, runs once
+  static byte was_measured = 0;
+  if (!was_measured) {
+    was_measured = 1;
+    unsigned long delta = micros() - time0;
+    long int audio_sample_rate = 1000000.0 * n / delta;
+    Serial.print("Computed audio sample rate: "); Serial.println(audio_sample_rate);
+  }
+
+  //debug
+  //Serial.println(sound_value);
+
+}
---
+++ sound_tests/12_PolyphonyMega_diffusion_thresholding/12_PolyphonyMega_diffusion_thresholding.ino
@@ -0,0 +1,191 @@
+//EndlessSynth, polyphony sine wave test using diffusion with thresholding method
+//- it's required to supress "high tone sound" occuring for silence in case of simple diffusion.
+
+//It supports 2,3,4 notes - set POLYPHONY define below.
+//After starting it plays increasing chord.
+//
+//Aditional libraries:
+//- install "TimerThree" using Library Manager
+//Device: Arduino Mega (Note: Not Uno!)
+//Commutation:
+//- buzzer at pin 2
+const byte pin_buz = 2;
+
+//Monitor port:
+//- baud must be set to 500000!
+//---------------------------------------------------------------
+
+#include <TimerThree.h>
+
+//use lower values if not enough computing power
+const long int audio_sample_rate = //44100;
+  //power of 2 to faster divide
+  8192;  //2^13
+  //4096;  //2^12  
+
+const long shift_audio = 13;
+    //12;
+
+//number of notes to play simultaneously, 1..4
+//#define POLYPHONY (2)  
+//#define POLYPHONY (3)  
+#define POLYPHONY (4)  
+
+//---------------------------------------------------------------
+void setup() {
+  Serial.begin(500000);
+  Serial.println("EndlessSynth Sine Wave Generator with Timer, v. 1.1 for Arduino Mega");
+
+  Serial.print("Buzzer pin: "); Serial.println(pin_buz);
+
+  Serial.print("Audio sample rate: "); Serial.println(audio_sample_rate);
+
+  //Wavetable
+  make_wavetable();
+
+
+  //Set port/pin mode for Arduino Mega for buzzer
+  //Ports in Arduino https://www.arduino.cc/en/Reference/PortManipulation
+  //Pins in Arduino Mega https://www.arduino.cc/en/Hacking/PinMapping2560
+  //pin 2 is 4 bit of Port E (in scheme PE2)
+  DDRE = B00010000;    //pinMode(2,OUTPUT);
+
+  //test_notes();
+  //Start interrupt for sound generation
+  Timer3.initialize(1000000 / audio_sample_rate);
+  Timer3.attachInterrupt(timer_interrupt);
+
+  Serial.print("Polyphony: "); Serial.println(POLYPHONY);
+}
+
+
+//---------------------------------------------------------------
+//Sine wavetable
+const int wave_n = 256;
+char wave_table[wave_n];
+
+void make_wavetable() {
+  int volume = 100; //0..127
+  for (int i = 0; i < wave_n; i++) {
+    wave_table[i] = int(sin(i * 2 * PI / wave_n) * volume);  //-127..127
+  }
+
+  /*Serial.println("Wavetable");
+  for (int i = 0; i < wave_n; i++) {
+    Serial.print(int(wave_table[i])); Serial.print(" ");
+    if (i > 0 && i % 20 == 0) Serial.println();
+  }
+  Serial.println();
+  */
+}
+
+
+//----------------------------------------------------------
+//frequencies of sounds
+long int freq1 = 440; 
+long int freq2 = 550; 
+long int freq3 = 660; 
+long int freq4 = 770;
+
+
+
+//----------------------------------------------------------
+//process timer interrupt event
+//sound generator
+long int t = 0;
+
+long int phase = 0;
+int sound_value = 0;
+const int diff_step = //127 * POLYPHONY; //step of diffusion subtraction - 1..127, kind of threshold for sound
+                    127;    //just 127 - to make polyphony sounding more "phatty"
+
+const int thresh_sound = diff_step / 2;
+
+
+//We should make this function as fast as possible, and trying to omit "/" and "%" operations
+void timer_interrupt() {
+  t++;
+
+  //phase - is changed audio_sample_rate times per second
+  //wave_n - length of wavetable
+  //freq - desired frequency
+  //ph - must go freq times 0..wave_n-1
+  sound_value += wave_table[(((long long)phase * freq1 * wave_n) >> shift_audio) % wave_n]; 
+#if POLYPHONY>=2
+  sound_value += wave_table[(((long long)phase * freq2 * wave_n) >> shift_audio) % wave_n]; 
+#endif
+#if POLYPHONY>=3
+  sound_value += wave_table[(((long long)phase * freq3 * wave_n) >> shift_audio) % wave_n]; 
+#endif
+#if POLYPHONY>=4
+  sound_value += wave_table[(((long long)phase * freq4 * wave_n) >> shift_audio) % wave_n]; 
+#endif
+
+  phase++;
+
+  //output audio sample to buzzer
+  //NOTE: silence generates 1/0/1/0 sequence in this approach:
+  /*if (sound_value >= 0) {
+    PORTE = B00010000; //buzzer ON
+    sound_value -= diff_step;       //diffusion propagation
+  }
+  else {
+    PORTE = B00000000; //buzzer OFF;
+    sound_value += diff_step;       //diffusion propagation
+  }*/
+  
+  //On removing 1/0/1/0 silence sequence problem:
+  //Idea to use threshold for hysteresis:
+  //>thresh -> HIGH,
+  //-thresh..thresh - do nothing
+  //<-thresh -> LOW.
+
+  if (sound_value >= thresh_sound) {
+    PORTE = B00010000; //buzzer ON
+    sound_value -= diff_step;       //diffusion propagation
+  }
+  else {
+    if (sound_value <= -thresh_sound) {
+      PORTE = B00000000; //buzzer OFF;
+      sound_value += diff_step;       //diffusion propagation
+    }
+  }
+}
+
+
+//----------------------------------------------------------
+void loop() {
+
+
+  long int time = millis();
+  //Serial.print("time: ");
+  //Serial.print(time);
+  //Serial.print("\t");
+  //Serial.print("counter: ");
+  //Serial.print(t);
+  Serial.print("\trate: ");
+  Serial.println(double(t) / (time / 1000.0));
+
+  static int note1 = 69-24; //midi bass note A
+  static int note2 = note1+12+3; //second note
+  static int note3 = note1+12+7; //second note
+  static int note4 = note1+12+12; //second note
+  freq1 = m_to_f(note1);
+  freq2 = m_to_f(note2);
+  freq3 = m_to_f(note1);
+  freq4 = m_to_f(note2);
+  Serial.print("notes "); Serial.print(note1); 
+  Serial.print(" "); Serial.println(note2);
+  
+  note1++;
+  note2++;
+  note3++;
+  note4++;
+  delay(500);
+
+}
+
+
+
+
+//---------------------------------------------------------------
