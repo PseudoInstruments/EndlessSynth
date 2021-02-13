bfa8dcf247a7bee6885b73a61265cc9a7e454d0c
perevalovds <perevalovds@gmail.com>
2021-02-13 09:03:24 +0500

    Synth with sliders

---
+++ synths/03_SineSynthArpegiatorMega_Sliders/03_SineSynthArpegiatorMega_Sliders.ino
@@ -0,0 +1,110 @@
+//EndlessSynth SineSynthArpegiatorMega, playing polyphonical sine wave using threshold+decay diffusion.
+//--------------------------------------------
+//Features:  
+//--------------------------------------------
+//- 3 polyphony
+//- sine wave synthesis
+//- without attack-release
+//- two sliders connected to A4 and A5 controls 1-bit algorithm.
+//    slider 1: "step" - kind of "level of sound generation", the higher - the more dithering
+//    slider 2: "keep" - memory of dithering, the higher - the more harshness
+
+//- the third slider controls sound volume.
+
+//--------------------------------------------
+//How to play:  
+//--------------------------------------------
+//Notes: 
+//   hold 1,2 or 3 notes by left hand and hit any of white rightest \"string\" keys.
+//   Combination of note key and string key plays a note, just liko on the guitar.
+//Octave switch: 
+//   use thee right black keys
+
+//--------------------------------------------
+//Board & Monitor port speed: 
+//--------------------------------------------
+//  Arduino Mega (Note: Not Uno!)
+//  Baud must be set to 500000!
+//--------------------------------------------
+//Aditional libraries:
+//- install "TimerThree" using Library Manager
+//--------------------------------------------
+//Conection:
+//--------------------------------------------
+//Buzzer of sound output: pin 2
+const byte pin_buz = 2;
+
+//Keyboard:
+//Toy Piano sensor was soldered prepared, so we have 32 out pins and pin for GND.
+//- 32 keys and Gnd from Toy Piano to Arduino Mega:
+//22,24,26,...,36,
+//23,25,27,...,37,
+//38,40,42,...,52,
+//39,41,43,...,53
+//and Gnd
+//(key numbers are reverted)
+
+//Sliders:
+//We are using board with three 10 kOhm potentiomeners,
+//Sliders 1,2 - step and diffusion of 1-bit algorithm.
+//  A4,A5 - output of two of them, 
+//  pin 4 as 5V, 
+//  pin 5 as Gnd.
+//Slider 3 - volume
+//  pin 4 (common with sliders 1,2) - Gnd 
+//  pin 2 - sound output
+
+//--------------------------------------------
+
+//---------------------------------------------------------------
+//number of notes to play simultaneously, 1..4
+#define POLYPHONY (3)
+//#define POLYPHONY (4) //For some reason 4 halts the system :)
+
+//print routines - use their for shortening code
+void pr(const char *str) {
+  Serial.print(str);
+}
+void prln(const char *str) {
+  Serial.println(str);
+}
+
+void pr(int i) {
+  Serial.print(i);
+}
+void prln(int i) {
+  Serial.println(i);
+}
+
+//---------------------------------------------------------------
+void setup() {
+  Serial.begin(500000);
+  Serial.println("----------------------------------------------------------------");  
+  Serial.println("EndlessSynth SineSynthArpegiatorMega_Sliders, v. 1.1 for Arduino Mega");
+  Serial.println("3 polyphony, sine wave synthesis. Without attack-release. Sliders controls 1-bit sound settings.");
+  Serial.println("How to play:  hold up to three notes by left hand and press 3 hit white \"string\" keys.");
+  Serial.println("Combination of note key and string key plays a note");
+  Serial.println("To switch octave use three right black keys");
+  Serial.println("----------------------------------------------------------------");  
+  
+  keyboard_setup();
+  sound_setup();
+  sliders_setup();
+
+  Serial.println();
+  Serial.println("Synth is ready to play.");
+}
+
+//----------------------------------------------------------
+void loop() {
+  sliders_loop();
+  keyboard_loop();
+  sound_loop();
+
+  delay(5); //2);
+}
+
+
+
+
+//---------------------------------------------------------------
---
+++ synths/03_SineSynthArpegiatorMega_Sliders/keyboard.ino
@@ -0,0 +1,141 @@
+//Toy Piano Keyboard Reader
+
+const int keys = 32;
+int keyReadPin[keys] = {22, 23, 24, 25, 26, 27, 28, 29,
+                        30, 31, 32, 33, 34, 35, 36, 37,
+                        38, 39, 40, 41, 42, 43, 44, 45,
+                        46, 47, 48, 49, 50, 51, 52, 53
+                       };
+
+const byte note_keys_count = keys - 7; //notes used for notes
+const byte string_keys_indices[POLYPHONY] = {31, 30, 28}; //, 26}; //"string" keys, note reversed order
+
+//keys for octave switch
+const byte key_octave1=25;  
+const byte key_octave2=27;  
+const byte key_octave3=29;  
+
+//octave base notes
+const byte note_octave1=65-3*12;
+const byte note_octave2=65-12;
+const byte note_octave3=65;   //F
+
+//Current base note - changed with octave switch
+byte base_note = note_octave2; 
+
+//---------------------------------------------------------------
+void keyboard_setup() {
+  //Set
+  for (int i = 0; i < keys; i++) {
+    pinMode(keyReadPin[i], INPUT_PULLUP);
+  }
+  //Print
+  Serial.print("Keys pins: ");
+  for (int i = 0; i < keys; i++) {
+    Serial.print(keyReadPin[i]); Serial.print(" ");
+  }
+  Serial.println();
+
+}
+
+//---------------------------------------------------------------
+byte key_state[keys]; //key state - pressed or released
+//TODO can store in a bit mask
+
+//pressed "note keys"
+byte note_keys[POLYPHONY];
+byte note_keys_n = 0;
+//mask of pressed "string keys"
+byte string_keys[POLYPHONY];
+
+//---------------------------------------------------------------
+//octave switch
+void set_base_note(char note) {
+  if (base_note != note) {
+    base_note = note;
+    Serial.print("Base note: ");
+    Serial.println(int(note));
+  }
+}
+
+//---------------------------------------------------------------
+//process pressing key
+void key_pressed(byte key) {
+  //this is note key
+  if (key < note_keys_count) {
+    if (note_keys_n < POLYPHONY) { //add to array
+      //Serial.print("note_key "); Serial.print(note_keys_n); Serial.print(" = "); Serial.println(key);
+      note_keys[note_keys_n++] = key;
+      //because we scan keys ordered, note_keys will be ordered too
+    }
+    return;
+  }
+
+  //this is string key
+  //TODO can map all keys
+  for (byte i = 0; i < POLYPHONY; i++) {
+    if (key == string_keys_indices[i]) {
+      //Serial.print("string_key "); Serial.println(i);
+      string_keys[i] = 1;
+      return;
+    }
+  }
+
+  //octave switch
+  if (key == key_octave1) { set_base_note(note_octave1); return; }
+  if (key == key_octave2) { set_base_note(note_octave2); return; }
+  if (key == key_octave3) { set_base_note(note_octave3); return; }
+  
+}
+
+//---------------------------------------------------------------
+//unsigned long int keyb_time_print_ = 0; //time for printing keyboard state
+
+void keyboard_loop() {
+  //clear notes and string keys
+  note_keys_n = 0;
+  for (byte i = 0; i < POLYPHONY; i++) {
+    note_keys[i] = -1;
+    string_keys[i] = 0;
+  }
+
+  bool was_changed = false;  //was keyboard state changed
+
+  //Scan keys
+  for (int key = keys - 1; key >= 0; key--) { //scan from right to left because of sorting "strings"
+    byte v = (digitalRead(keyReadPin[keys - 1 - key]) == LOW) ? 1 : 0;
+    byte &state = key_state[key];
+    //key was pressed or released
+    if (state != v) {
+      was_changed = true;
+      Serial.print("key "); Serial.print(key); Serial.print(": ");
+      if (v) Serial.println("on");
+      else Serial.println("off");
+    }
+    state = v;
+
+    //process pressing events
+    if (v) {
+      key_pressed(key);
+    }
+  }
+
+
+  //play
+  if (was_changed) {
+    //set keys with using base_note, which is depends on choosen octave
+    set_notes(string_keys[0] ? note_keys[0] : -1,
+              string_keys[1] ? note_keys[1] : -1,
+              string_keys[2] ? note_keys[2] : -1,
+              string_keys[3] ? note_keys[3] : -1,
+              base_note);
+  }
+  //test print of keyboard state - use for check all is connected
+  //for (int i=0; i<keys; i++) {
+  //  Serial.print(key_state[i]);
+  //}
+  //Serial.println();
+
+}
+
+//---------------------------------------------------------------
